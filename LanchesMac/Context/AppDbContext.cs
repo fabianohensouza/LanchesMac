b90dae909a651dff803d30a18b567c997cee2ef0
Fabiano Souza <fabianohensouza@gmail.com>
2023-12-03 12:01:22 -0300

    Configuring the database connection with EFCore 8

--- LanchesMac/Context/AppDbContext.cs
+++ LanchesMac/Context/AppDbContext.cs
@@ -1,8 +1,16 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using LanchesMac.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace LanchesMac.Context
 {
     public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) 
+            : base(options)
+        {            
+        }
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Lanche> Lanches { get; set; }
     }
 }
