1654a6aaaacfded561e564c8f6ea373c33a0733f
Fabiano Souza <fabianohensouza@gmail.com>
2023-12-04 07:44:41 -0300

    Configuring Data Annotations

--- LanchesMac/Models/Lanche.cs
+++ LanchesMac/Models/Lanche.cs
@@ -1,9 +1,20 @@
-﻿namespace LanchesMac.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LanchesMac.Models
 {
+    [Table("Lanches")]
     public class Lanche
     {
+        [Key]
         public int LancheId { get; set; }
+        [Required(ErrorMessage = "O nome do lanche deve ser informado")]
+        [Display(Name = "Nome do Lanche")]
         public string Nome { get; set; }
+        [Required(ErrorMessage = "A descrição do lanche deve ser informada")]
+        [Display(Name = "Descrição do Lanche")]
+        [MinLength(20, ErrorMessage = "A descrição deve ter no mínimo {1} caracteres")]
+        [MaxLength(200, ErrorMessage = "A descrição deve ter no máximo {1} caracteres")]
         public string DescricaoCurta { get; set; }
         public string DescricaoDetalhada { get; set; }
         public decimal Preco { get; set; }
