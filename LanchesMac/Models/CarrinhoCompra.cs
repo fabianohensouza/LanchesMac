b36929952e41c2cc8b2af506687c68b6907a9ca5
Fabiano Souza <fabianohensouza@gmail.com>
2023-12-15 07:24:25 -0300

    Strating create the shopping cart

---
+++ LanchesMac/Models/CarrinhoCompra.cs
@@ -0,0 +1,61 @@
+﻿using LanchesMac.Context;
+using Microsoft.AspNetCore.Http;
+
+namespace LanchesMac.Models
+{
+    public class CarrinhoCompra
+    {
+        private readonly AppDbContext _context;
+
+        public CarrinhoCompra(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public string CarrinhoCompraId { get; set; }
+        public List<CarrinhoCompraItem> CarrinhoCompraItens { get; set; }
+
+        public static CarrinhoCompra GetCarrinho(IServiceProvider services)
+        {
+            ISession session = 
+                services.GetService<IHttpContextAccessor>()?.HttpContext.Session;
+
+            var context = services.GetService<AppDbContext>();
+
+            string carrinhoId = session.GetString("CarrinhoID") ?? Guid.NewGuid().ToString();
+
+            session.SetString("CarrinhoID", carrinhoId);
+
+            return new CarrinhoCompra(context)
+            {
+                CarrinhoCompraId = carrinhoId
+            };
+        }
+
+        public void AdicionarAoCarrinho(Lanche lanche)
+        {
+            var carrinhoCompraItem =
+                _context.CarrinhoCompraItens.SingleOrDefault(x =>
+                    x.Lanche.LancheId == lanche.LancheId &&
+                    x.CarrinhoCompraId == CarrinhoCompraId);
+
+            if (carrinhoCompraItem == null)
+            {
+                carrinhoCompraItem = new CarrinhoCompraItem
+                {
+                    CarrinhoCompraId = CarrinhoCompraId,
+                    Lanche = lanche,
+                    Quantity = 1
+                };
+
+                _context.CarrinhoCompraItens.Add(carrinhoCompraItem);
+            }
+            else
+            {
+                carrinhoCompraItem.Quantity++;
+            }
+
+            _context.SaveChanges();
+        }
+    }
+}
