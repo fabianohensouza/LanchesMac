1a317f3fdcfa2a6eb72498a1fd2c6b69241d5873
Fabiano Souza <fabianohensouza@gmail.com>
2023-12-21 07:39:33 -0300

    Creating a ViewComponent to show the cart products in the NavBar

---
+++ LanchesMac/Components/CarrinhoCompraResumo.cs
@@ -0,0 +1,31 @@
+﻿using LanchesMac.Models;
+using LanchesMac.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Components
+{
+    public class CarrinhoCompraResumo : ViewComponent
+    {
+        private readonly CarrinhoCompra _carrinhoCompra;
+
+        public CarrinhoCompraResumo(CarrinhoCompra carrinhoCompra)
+        {
+            _carrinhoCompra = carrinhoCompra;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var itens = _carrinhoCompra.GetCarrinhoCompraItems();
+            
+            _carrinhoCompra.CarrinhoCompraItens = itens;
+
+            var carrinhoCompraVM = new CarrinhoCompraViewModel
+            {
+                CarrinhoCompra = _carrinhoCompra,
+                CarrinhoCompraTotal = _carrinhoCompra.GetCarrinhoCompraTotal()
+            };
+
+            return View(carrinhoCompraVM);
+        }
+    }
+}
