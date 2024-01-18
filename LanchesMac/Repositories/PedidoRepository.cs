d45c9f3a9b4b5d4289357f77bfb68bc58c3fdc06
Fabiano Souza <fabianohensouza@gmail.com>
2024-01-18 07:34:52 -0300

    Configuring the order repository

---
+++ LanchesMac/Repositories/PedidoRepository.cs
@@ -0,0 +1,42 @@
+﻿using LanchesMac.Context;
+using LanchesMac.Models;
+using LanchesMac.Repositories.Interfaces;
+
+namespace LanchesMac.Repositories
+{
+    public class PedidoRepository : IPedidoRepository
+    {
+        private readonly AppDbContext _appDbContext;
+        private readonly CarrinhoCompra _carrinhoCompra;
+
+        public PedidoRepository(
+            AppDbContext appDbContext, 
+            CarrinhoCompra carrinhoCompra)
+        {
+            _appDbContext = appDbContext;
+            _carrinhoCompra = carrinhoCompra;
+        }
+
+        public void CriarPedido(Pedido pedido)
+        {
+            pedido.PedidoEnviado = DateTime.Now;
+            _appDbContext.Pedidos.Add(pedido);
+            _appDbContext.SaveChanges();
+
+            var carrinhoCompraItens = _carrinhoCompra.CarrinhoCompraItens;
+
+            foreach (var item in carrinhoCompraItens)
+            {
+                var pedidoDetail = new PedidoDetalhe()
+                {
+                    Quantidade = item.Quantity,
+                    LancheId = item.Lanche.LancheId,
+                    PedidoId =  pedido.PedidoId,
+                    Preco = item.Lanche.Preco
+                };
+                _appDbContext.PedidoDetalhes.Add(pedidoDetail);
+            }
+            _appDbContext.SaveChanges();
+        }
+    }
+}
