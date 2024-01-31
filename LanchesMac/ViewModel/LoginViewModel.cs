8dcd0ee0de5cd9ba13b39f7fe09d4e4471090dd6
Fabiano Souza <fabianohensouza@gmail.com>
2024-01-31 07:34:23 -0300

    Implementing login controller and view

---
+++ LanchesMac/ViewModel/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace LanchesMac.ViewModel
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Informe o login do usuário")]
+        [Display(Name = "Usuário")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Informe a senha")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Senha")]
+        public string Password { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
