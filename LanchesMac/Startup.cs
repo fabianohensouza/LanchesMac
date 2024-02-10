9b54ba2041e3d23772b9931b2ce9743a6469d8dc
Fabiano Souza <fabianohensouza@gmail.com>
2024-02-10 11:09:10 -0300

    Configuring the access  roles and attributions

--- LanchesMac/Startup.cs
+++ LanchesMac/Startup.cs
@@ -2,6 +2,7 @@
 using LanchesMac.Models;
 using LanchesMac.Repositories;
 using LanchesMac.Repositories.Interfaces;
+using LanchesMac.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -29,7 +30,9 @@ public class Startup
         services.AddTransient<ICategoriaRepository, CategoriaRepository>();
         services.AddTransient<IPedidoRepository, PedidoRepository>();
         services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
         services.AddScoped(sp => CarrinhoCompra.GetCarrinho(sp));
+        services.AddScoped<ISeedUserRoleInitial, SeedUserRoleInitial>(); ;
 
         services.AddControllersWithViews();
 
@@ -38,7 +41,10 @@ public class Startup
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    public void Configure(
+        IApplicationBuilder app, 
+        IWebHostEnvironment env,
+        ISeedUserRoleInitial seedUserRoleInitial)
     {
         if (env.IsDevelopment())
         {
@@ -54,6 +60,10 @@ public class Startup
 
         app.UseStaticFiles();
         app.UseRouting();
+
+        seedUserRoleInitial.SeedRoles();
+        seedUserRoleInitial.SeedUsers();
+
         app.UseSession();
 
         app.UseAuthentication();
